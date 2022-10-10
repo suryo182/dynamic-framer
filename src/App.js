738a103191499474773317261c064164bde3f307
suryo182 <suryo182@gmail.com>
2022-10-11 06:42:26 +0700

    use transform with drag x

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import React, {
-  useCallback,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { useCallback, useLayoutEffect, useRef, useState } from 'react';
 import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
 
 function App() {
   const ghostRefY = useRef();
   const scrollXRef = useRef();
-  const [viewportH, setViewportH] = useState(0);
+  // const [viewportH, setViewportH] = useState(0);
+  const [transformY, setTransformY] = useState(0);
   const { scrollYProgress } = useScroll();
-  const { scrollXProgress } = useScroll({ container: scrollXRef });
-  const [scrollRange, setScrollRange] = useState(0);
-  const transform = useTransform(
-    scrollXProgress,
-    [0, 1],
-    [0, -viewportH + scrollRange]
-  );
-  const physics = { damping: 15, mass: 0.27, stiffness: 55 };
-  const spring = useSpring(transform, physics);
+  // const { scrollXProgress } = useScroll({ container: scrollXRef });
+  // const [scrollRange, setScrollRange] = useState(0);
+  // const transform = useTransform(
+  //   scrollXProgress,
+  //   [0, 1],
+  //   [0, -viewportH + scrollRange]
+  // );
+  // const physics = { damping: 15, mass: 0.27, stiffness: 55 };
+  // const spring = useSpring(transform, physics);
 
-  useLayoutEffect(() => {
-    scrollXRef && setScrollRange(scrollXRef.current.scrollWidth);
-  }, [scrollXRef]);
+  // useLayoutEffect(() => {
+  //   scrollXRef && setScrollRange(scrollXRef.current.scrollWidth);
+  // }, [scrollXRef]);
 
-  const onResize = useCallback((entries) => {
-    for (let entry of entries) {
-      setViewportH(entry.contentRect.height);
-    }
-  }, []);
+  // const onResize = useCallback((entries) => {
+  //   for (let entry of entries) {
+  //     setViewportH(entry.contentRect.height);
+  //   }
+  // }, []);
 
-  useLayoutEffect(() => {
-    const resizeObserver = new ResizeObserver((entries) => onResize(entries));
-    resizeObserver.observe(ghostRefY.current);
-    return () => resizeObserver.disconnect();
-  }, [onResize]);
+  // useLayoutEffect(() => {
+  //   const resizeObserver = new ResizeObserver((entries) => onResize(entries));
+  //   resizeObserver.observe(ghostRefY.current);
+  //   return () => resizeObserver.disconnect();
+  // }, [onResize]);
 
   return (
     <div ref={ghostRefY} className="h-screen">
       <motion.div
-        style={{ y: spring }}
+        style={{ y: transformY }}
         className="p-8 h-max flex flex-col gap-y-4"
       >
         <div className="bg-red-500 h-36">Red</div>
@@ -50,12 +46,16 @@ function App() {
         <div className="bg-orange-500 h-36">Orange</div>
         <div className="bg-pink-500 h-36">Pink</div>
       </motion.div>
-      <Nav scrollYProgress={scrollYProgress} scrollXRef={scrollXRef} />
+      <Nav
+        scrollYProgress={scrollYProgress}
+        scrollXRef={scrollXRef}
+        setTransformY={setTransformY}
+      />
     </div>
   );
 }
 
-const Nav = ({ scrollYProgress, scrollXRef }) => {
+const Nav = ({ scrollYProgress, scrollXRef, setTransformY }) => {
   const ghostRef = useRef();
   const scrollRef = useRef();
   const [scrollRange, setScrollRange] = useState(0);
@@ -87,12 +87,15 @@ const Nav = ({ scrollYProgress, scrollXRef }) => {
   return (
     <motion.div
       ref={scrollXRef}
-      className="fixed bottom-4 w-[80%] transform -translate-x-1/2 left-1/2 h-max rounded-2xl bg-green-200 cursor-pointer overflow-x-auto"
+      className="fixed bottom-4 w-[80%] transform -translate-x-1/2 left-1/2 h-max rounded-2xl bg-green-200 cursor-pointer overflow-x-hidden"
     >
       <div ref={scrollRef}>
         <motion.div
           drag="x"
           style={{ x: spring }}
+          onDrag={(event, pan) => {
+            setTransformY(pan.offset.x);
+          }}
           dragConstraints={scrollRef}
           className="flex gap-x-1 w-max px-4"
         >
